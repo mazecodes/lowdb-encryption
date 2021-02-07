2eb382afdf11823b4f017b6712af31ba3e792802
Maze Peterson <mazecodes@protonmail.com>
2021-02-07 04:06:54 -0800

    Update crypto sandbox

--- sandbox/crypto.js
+++ sandbox/crypto.js
@@ -3,8 +3,12 @@ const crypto = require('../src/lib/crypto');
 const salt = crypto.generateSalt();
 const key = crypto.generateKey('secret', salt);
 const { state, signature } = crypto.encryptState('{ foo: "bar" }', key);
+const isStateValid = crypto.isStateValid(state, signature, key);
+const decryptedState = crypto.decryptState(state, key);
 
-console.log(salt);
-console.log(key);
-console.log(state);
-console.log(signature);
+console.log('Salt', salt);
+console.log('Key', key);
+console.log('Encrypted state', state);
+console.log('Signature', signature);
+console.log('Is state valid', isStateValid);
+console.log('Decrypted state', decryptedState);
