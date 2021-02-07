15344facc92abc8b9b61b4041196a3add5493eb2
Maze Peterson <mazecodes@protonmail.com>
2021-02-07 03:56:03 -0800

    Add encrypt state method

--- sandbox/crypto.js
+++ sandbox/crypto.js
@@ -2,6 +2,9 @@ const crypto = require('../src/lib/crypto');
 
 const salt = crypto.generateSalt();
 const key = crypto.generateKey('secret', salt);
+const { state, signature } = crypto.encryptState('{ foo: "bar" }', key);
 
 console.log(salt);
 console.log(key);
+console.log(state);
+console.log(signature);
--- src/lib/crypto.js
+++ src/lib/crypto.js
@@ -31,7 +31,30 @@ const generateKey = (secret, salt, iterations = 100000) =>
     iterations,
   }).toString(CryptoJS.enc.Hex);
 
+/**
+ * Encrypt the given state
+ *
+ * @param {String} state - The state to encrypt
+ * @param {String} key - The key to be used for encryption
+ * @returns {Object} - Encrypted state and signature
+ *
+ * @example
+ *   encryptState('{ foo: "bar" }', 'key')
+ */
+const encryptState = (state, key) => {
+  const encryptedState = CryptoJS.AES.encrypt(state, key).toString();
+  const signature = CryptoJS.HmacSHA256(encryptedState, key).toString(
+    CryptoJS.enc.hex
+  );
+
+  return {
+    state: encryptedState,
+    signature,
+  };
+};
+
 module.exports = {
   generateSalt,
   generateKey,
+  encryptState,
 };
