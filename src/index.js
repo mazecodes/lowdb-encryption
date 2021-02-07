fe6dd23b75742284413f4bce4aeeef692c33346a
Maze Peterson <mazecodes@protonmail.com>
2021-02-07 04:17:04 -0800

    Add serialize method

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { validateOptions } = require('./lib/validator');
+const crypto = require('./lib/crypto');
 
 const lowdbEncryption = (options = {}) => {
   validateOptions(options);
@@ -6,9 +7,32 @@ const lowdbEncryption = (options = {}) => {
   const { secret } = options;
   const iterations = options.iterations || 100000;
 
+  const salt = crypto.generateSalt();
+  let key;
+
   return {
-    serialize() {
-      console.log('Hello World');
+    /**
+     * Encryption
+     */
+    serialize(data) {
+      if (!key) {
+        key = crypto.generateKey(secret, salt, iterations);
+      }
+
+      const { state, signature } = crypto.encryptState(data);
+
+      const stateObject = {
+        _encryption: {
+          salt,
+          iterations,
+        },
+        state: {
+          content: state,
+          signature,
+        },
+      };
+
+      return JSON.stringify(stateObject);
     },
   };
 };
